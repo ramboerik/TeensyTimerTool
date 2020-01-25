6c5b3d7630fe08b973ee923610dd966bb969d759
Erik tideman <erik.tideman@gmail.com>
2020-01-25 12:47:05 +0100

    Renamed timer.h/.cpp -> TennesyTimer.h/.cpp for all boards.
    platformio gets confused with "timer.h" as there is also a "timer.h" in some arduino library implementations

--- src/TeensyTimerTool.h
+++ src/TeensyTimerTool.h
@@ -6,45 +6,47 @@
 // Board specifics
 
 #if defined(TLC)
-    #include "timer.h"
+    #include "TeensyTimer.h"
    #include "ErrorHandling/ErrorCodes.h"
     #include "Teensy/TCK/TCK.h"
 #elif defined(T3_0)
-    #include "timer.h"
+    #include "TeensyTimer.h"
     #include "ErrorHandling/ErrorCodes.h"
     #include "Teensy/TCK/TCK.h"
     #include "Teensy/FTM/FTM.h"
 
 #elif defined(T3_1) || defined (T3_2)
     #include "kinetis.h"
-    #include "timer.h"
+    #include "TeensyTimer.h"
     #include "ErrorHandling/ErrorCodes.h"
     #include "Teensy/TCK/TCK.h"
     #include "Teensy/FTM/FTM.h"
 
 #elif defined (T3_5)
     #include "kinetis.h"
-    #include "timer.h"
+    #include "TeensyTimer.h"
     #include "ErrorHandling/ErrorCodes.h"
     #include "Teensy/TCK/TCK.h"
     #include "Teensy/FTM/FTM.h"
 
 #elif defined (T3_6)
     #include "kinetis.h"
-    #include "timer.h"
+    #include "TeensyTimer.h"
    #include "ErrorHandling/ErrorCodes.h"
     #include "Teensy/TCK/TCK.h"
     #include "Teensy/FTM/FTM.h"
 
 #elif defined(T4_0)
     #include <imxrt.h>
-    #include "timer.h"
+    #include "TeensyTimer.h"
     #include "ErrorHandling/ErrorCodes.h"
     #include "Teensy/TCK/TCK.h"
     #include "Teensy/GPT/GPT.h"
     #include "Teensy/TMR/TMR.h"
 
 #elif defined(STM32F4xx)
+    #include "TeensyTimer.h"
+    #include "ErrorHandling/ErrorCodes.h"
     #include "STM32F4/TCK/TCK.h"
     //#include "STM32F4/GPT/GPT.h"
 
